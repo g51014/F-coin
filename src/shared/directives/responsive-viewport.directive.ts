beb2fd8551a77d3f2d3b689003766d4ebc4b8f5a
g51014 <a912686931@gmail.com>
2021-03-22 00:50:32 +0800

    responsive-viewport add buffer

--- src/shared/directives/responsive-viewport.directive.ts
+++ src/shared/directives/responsive-viewport.directive.ts
@@ -1,14 +1,17 @@
 import { ResizeObserver } from 'resize-observer';
 import { tap } from 'rxjs/operators';
-import { Directive, ElementRef, Renderer2, OnInit } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, Input } from '@angular/core';
 import { WindowService } from '@services/window.service';
 import { UnsubOndestroy } from '@utilities/abstract/unsub-ondestroy';
 import { EDeviceType } from '@utilities/enums/overlay.enum';
 
 @Directive({
-  selector: '[appResponsiveViewport]'
+  selector: '[appResponsiveViewport]',
 })
-export class ResponsiveViewportDirective extends UnsubOndestroy implements OnInit {
+export class ResponsiveViewportDirective
+  extends UnsubOndestroy
+  implements OnInit {
+  @Input() responseBuffer: Element[] = [];
 
   constructor(
     private e: ElementRef,
@@ -19,12 +22,15 @@ export class ResponsiveViewportDirective extends UnsubOndestroy implements OnIni
   }
 
   private resize$: ResizeObserver;
+  get bufferHeight() { return this.responseBuffer.reduce((total, current) => total = total + current.clientHeight, 0); }
 
   ngOnInit() {
-    this.resize$ = this.$window.windowRsizeObserver(this.resizeViewport.bind(this));
-    this.$window.device$.pipe(
-      tap(device => this.onDeviceChanged(device))
-    ).subscribe();
+    this.resize$ = this.$window.windowRsizeObserver(
+      this.resizeViewport.bind(this)
+    );
+    this.$window.device$
+      .pipe(tap((device) => this.onDeviceChanged(device)))
+      .subscribe();
   }
 
   private onDeviceChanged(device: EDeviceType) {
@@ -32,12 +38,15 @@ export class ResponsiveViewportDirective extends UnsubOndestroy implements OnIni
       this.resize$.observe(this.e.nativeElement);
     } else {
       this.resize$.disconnect();
-      this.render.removeStyle(this.e.nativeElement, 'height')
+      this.render.removeStyle(this.e.nativeElement, 'height');
     }
   }
 
   private resizeViewport() {
-    this.render.setStyle(this.e.nativeElement, 'height', `${this.$window.windowHeight}px`);
+    this.render.setStyle(
+      this.e.nativeElement,
+      'height',
+      `${this.$window.windowHeight - this.bufferHeight}px`
+    );
   }
-
 }
